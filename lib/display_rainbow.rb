2ae916cd4edf44092735f86bb28144745df4471b
Crystal <cdchutchinson@gmail.com>
2020-03-06 05:38:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow colors= ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-  puts "rainbow: #{colors}"
+  puts "rainbow: #{["0", "1", "2", "3", "4", "5", "6"]}
 end
